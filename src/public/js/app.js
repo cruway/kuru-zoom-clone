c25d0a30681bf3f2080b2d37f097260099cf9798
sim wook <astralkaiser89@gmail.com>
2022-10-22 20:40:22 +0900

    websocket messages

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,13 +1,17 @@
 const socket = new WebSocket(`ws:/${window.location.host}`);
 
 socket.addEventListener("open", () => {
-    console.log("Connected to Browser");
+    console.log("Connected to Server ✅");
 });
 
 socket.addEventListener("message", (message) => {
-    console.log("Just got this: ", message, " from the Server");
+    console.log("New message: ", message.data)
 });
 
 socket.addEventListener("close", () => {
-    console.log("Connected from Server ❌");
+    console.log("Disconnected from Server ❌");
 });
+
+setTimeout(() => {
+    socket.send("hello from the browser!");
+}, 10000);
