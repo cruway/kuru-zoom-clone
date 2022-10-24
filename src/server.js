db025e2d1aba1eb94cfc6da797c0f6c8379cdb60
sim wook <astralkaiser89@gmail.com>
2022-10-24 22:13:29 +0900

    websocket messages

--- src/server.js
+++ src/server.js
@@ -19,14 +19,15 @@ function onSocketClose() {
     console.log("Disconnected from the Browser ❌");
 }
 
-function onSocketMessage(message) {
-    console.log(message.toString('utf8'));
-}
+const sockets = [];
 
 wss.on("connection", (socket) => {
+    sockets.push(socket);
     console.log("Connected to Browser ✅");
     socket.on("close", onSocketClose);
-    socket.on("message", onSocketMessage);
+    socket.on("message", message => {
+        sockets.forEach(aSocket => aSocket.send(message.toString()));
+    });
     socket.send("hello!!!");
 });
 
